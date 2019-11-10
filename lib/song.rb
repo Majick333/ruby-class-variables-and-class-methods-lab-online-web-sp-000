b8201e241e5ef5da8b2daab16052d4caeb39e247
Ryan M <rmccauleymusic@gmail.com>
2019-11-10 21:45:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,14 +18,25 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name,
-    @artist = artist
-    @genre = genre
-    @@artist << artist
-    @@genres << genre
+    @artist = artist,
+    @genre = genre,
+    @@artist << artist,
+    @@genres << genre,
     @@count += 1
   end
 
   attr_accessor :name, :artist, :genre
+
+  def self.genre_count
+    @@genre_count.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count = 1
+      end
+    end
+    genre_count
+  end
 
 
 end
